f98ee837bf74177b58155c0e9567881d139f6572
penrose15 <seongju99@naver.com>
2022-08-23 01:09:41 +0900

    add validation

--- src/main/java/com/ToDoList/todo/TodoController.java
+++ src/main/java/com/ToDoList/todo/TodoController.java
@@ -5,11 +5,15 @@ import com.ToDoList.todo.dto.PostTodoDto;
 import com.ToDoList.todo.dto.ResponseTodoDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.ArrayList;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
@@ -23,21 +27,21 @@ public class TodoController {
     }
     //todo 생성
     @PostMapping
-    public ResponseEntity createTodo(@RequestBody PostTodoDto postTodoDto) {
+    public ResponseEntity createTodo(@RequestBody @Valid PostTodoDto postTodoDto) {
         Todo todo = todoService.create(mapper.postTodoDtoToTodo(postTodoDto));
         return new ResponseEntity<>(mapper.todoToResponseTodoDto(todo),HttpStatus.CREATED);
     }
     //todo 수정
     @PatchMapping("/{todo-id}")
-    public ResponseEntity updateTodo(@PathVariable("todo-id") Long todoId,
-                                     @RequestBody PatchTodoDto patchTodoDto) {
+    public ResponseEntity updateTodo(@PathVariable("todo-id") @Positive Long todoId,
+                                     @RequestBody @Valid PatchTodoDto patchTodoDto) {
         patchTodoDto.setTodoId(todoId);
         Todo todo = todoService.update(mapper.patchTodoDtoToTodo(patchTodoDto));
         return new ResponseEntity<>(mapper.todoToResponseTodoDto(todo),HttpStatus.OK);
     }
     //todo 조회
     @GetMapping("/{todo-id}")
-    public ResponseEntity getTodoById(@PathVariable("todo-id") Long todoId) {
+    public ResponseEntity getTodoById(@PathVariable("todo-id") @Positive Long todoId) {
         Todo todo = todoService.findTodoById(todoId);
         return new ResponseEntity<>(mapper.todoToResponseTodoDto(todo), HttpStatus.OK);
     }
@@ -49,7 +53,7 @@ public class TodoController {
     }
     //todo 삭제
     @DeleteMapping("/{todo-id}")
-    public ResponseEntity deleteTodo(@PathVariable("todo-id")Long todoId) {
+    public ResponseEntity deleteTodo(@PathVariable("todo-id") @Positive Long todoId) {
         todoService.delete(todoId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
